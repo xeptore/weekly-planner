c659769941ff295ee57e0ba755fe185c8ec86a0d
morteza behboodian <xeptore@gmail.com>
2019-08-24 19:39:43 +0430

    move states to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,75 @@
 <script>
   import Table from "./components/Table.svelte";
+  import Buttons from "./components/Buttons.svelte";
+
+  import range from "lodash/range";
+  import uuid from "uuid/v4";
+  import { cloneDeepRows } from "./util";
+
+  // 0 -> odd
+  // 1 -> all
+  // 2 -> even
+  let filter = 1;
+
+  const rest = range(6).map(() => "-");
+  let rows = [
+    {
+      day: "Saturday",
+      data: [...rest],
+      key: uuid()
+    },
+    {
+      day: "Sunday",
+      data: [...rest],
+      key: uuid()
+    },
+    {
+      day: "Monday",
+      data: [...rest],
+      key: uuid()
+    },
+    {
+      day: "Tuesday",
+      data: [...rest],
+      key: uuid()
+    },
+    {
+      day: "Wednesday",
+      data: [...rest],
+      key: uuid()
+    },
+    {
+      day: "Thursday",
+      data: [...rest],
+      key: uuid()
+    },
+    {
+      day: "Friday",
+      data: [...rest],
+      key: uuid()
+    }
+  ];
+
+  const handleFilterChange = ({ detail }) => {
+    filter = detail.selected;
+  };
+
+  const handleTableCellChange = ({ detail }) => {
+    rows = cloneDeepRows(rows, detail);
+  };
+
+  $: {
+    console.log(rows);
+  }
 </script>
 
 <style>
-  .header {
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-  }
-
-  .header.header--text-center {
-    text-align: center;
+  .wrapper {
+    margin-top: 1rem;
   }
 </style>
 
-<div class="ui container">
-  <div class="header header--text-center">
-    <h1 class="header__title">Welcome!</h1>
-  </div>
-  <Table />
+<div class="ui container wrapper">
+  <Table {rows} on:change={handleTableCellChange} />
+  <Buttons selected={filter} on:change={handleFilterChange} />
 </div>
