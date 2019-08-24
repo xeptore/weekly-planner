d9bf8aad875cbdf673d66ba12cb28d338976b972
morteza behboodian <xeptore@gmail.com>
2019-08-24 19:40:01 +0430

    add Buttons.svelte filter buttons group

---
+++ src/components/Buttons.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let selected = 1;
+
+  const dispatch = createEventDispatcher();
+
+  const buildClickHandler = x => () => {
+    console.log(`${x} has been clicked!`);
+    dispatch("change", {
+      selected: x
+    });
+  };
+</script>
+
+<style>
+  .buttons-container {
+    margin-top: 1.5rem;
+  }
+</style>
+
+<div class="ui centered grid">
+  <div class="buttons-container">
+    <button
+      class="large ui black button"
+      on:click={buildClickHandler(0)}
+      class:positive={selected === 0}>
+      Odd
+    </button>
+    <button
+      class="large ui black button"
+      on:click={buildClickHandler(1)}
+      class:positive={selected === 1}>
+      Fixed
+    </button>
+    <button
+      class="large ui black button"
+      on:click={buildClickHandler(2)}
+      class:positive={selected === 2}>
+      Even
+    </button>
+  </div>
+</div>
