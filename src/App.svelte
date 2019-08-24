d61b42ebee4e831711eecab399e6f9b0b028dae0
morteza behboodian <xeptore@gmail.com>
2019-08-24 22:40:14 +0430

    remove initial states from App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,54 +1,11 @@
 <script>
+  import { cloneDeepRows } from "./util";
+
   import Table from "./components/Table.svelte";
   import Buttons from "./components/Buttons.svelte";
 
-  import range from "lodash/range";
-  import uuid from "uuid/v4";
-  import { cloneDeepRows } from "./util";
-
-  // 0 -> odd
-  // 1 -> all
-  // 2 -> even
-  let filter = 1;
-
-  const rest = range(6).map(() => "-");
-  let rows = [
-    {
-      day: "Saturday",
-      data: [...rest],
-      key: uuid()
-    },
-    {
-      day: "Sunday",
-      data: [...rest],
-      key: uuid()
-    },
-    {
-      day: "Monday",
-      data: [...rest],
-      key: uuid()
-    },
-    {
-      day: "Tuesday",
-      data: [...rest],
-      key: uuid()
-    },
-    {
-      day: "Wednesday",
-      data: [...rest],
-      key: uuid()
-    },
-    {
-      day: "Thursday",
-      data: [...rest],
-      key: uuid()
-    },
-    {
-      day: "Friday",
-      data: [...rest],
-      key: uuid()
-    }
-  ];
+  export let filter = "";
+  export let rows = [];
 
   const handleFilterChange = ({ detail }) => {
     filter = detail.selected;
@@ -57,10 +14,6 @@
   const handleTableCellChange = ({ detail }) => {
     rows = cloneDeepRows(rows, detail);
   };
-
-  $: {
-    console.log(rows);
-  }
 </script>
 
 <style>
@@ -70,6 +23,6 @@
 </style>
 
 <div class="ui container wrapper">
-  <Table {rows} on:change={handleTableCellChange} />
+  <Table {rows} {filter} on:change={handleTableCellChange} />
   <Buttons selected={filter} on:change={handleFilterChange} />
 </div>
