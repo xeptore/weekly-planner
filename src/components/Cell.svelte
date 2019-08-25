51bee979dae63cc0ab673973fab59877028d7936
morteza behboodian <xeptore@gmail.com>
2019-08-25 09:06:02 +0430

    improve cell input styling

--- src/components/Cell.svelte
+++ src/components/Cell.svelte
@@ -1,19 +1,19 @@
 <script>
-  import { tick, createEventDispatcher } from "svelte";
+  import { tick, createEventDispatcher } from 'svelte';
 
   const dispatch = createEventDispatcher();
 
-  export let value = "-";
-  export let day = "";
+  export let value = '-';
+  export let day = '';
   export let index = 0;
   export let filter = '';
   let inTypeMode = false;
 
-  const handleDoubleClick = async e => {
+  const handleDoubleClick = async () => {
     inTypeMode = true;
 
     await tick();
-    document.getElementById("input").focus();
+    document.getElementById('input').focus();
   };
   const handleSubmit = () => {
     if (!inTypeMode) {
@@ -21,14 +21,14 @@
     }
 
     inTypeMode = false;
-    dispatch("change", {
+    dispatch('change', {
       value,
       index,
       day,
       type: filter,
     });
   };
-  const handleFocus = e => {
+  const handleFocus = (e) => {
     e.target.setSelectionRange(0, e.target.value.length);
   };
 </script>
@@ -39,21 +39,29 @@
     width: 100%;
   }
 
+  .input-wrapper,
+  .ui.input > input,
   .form--input {
     text-align: center;
   }
 </style>
 
 <td class="center aligned selectable" on:dblclick={handleDoubleClick}>
-  {#if inTypeMode}
-    <form on:submit|preventDefault|stopPropagation={handleSubmit} class="form">
-      <input
-        id="input"
-        bind:value
-        class="form--input"
-        on:focus={handleFocus}
-        on:blur={handleSubmit}
-        type="text" />
+  {#if inTypeMode && value !== 'X'}
+    <form
+      on:submit|preventDefault|stopPropagation={handleSubmit}
+      class="form"
+      autocomplete="off">
+      <div class="ui input focus input-wrapper">
+        <input
+          id="input"
+          autocomplete="off"
+          bind:value
+          class="form--input"
+          on:focus={handleFocus}
+          on:blur={handleSubmit}
+          type="text" />
+      </div>
     </form>
   {:else}{value}{/if}
 </td>
