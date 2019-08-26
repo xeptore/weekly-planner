c3530c2cf46b93d11c25d28903186c6cf522396f
morteza behboodian <xeptore@gmail.com>
2019-08-26 20:03:45 +0430

    code style

--- src/components/Info.svelte
+++ src/components/Info.svelte
@@ -4,7 +4,7 @@
     color: rgba(0, 0, 0, 0.75);
   }
 
-  .icon:hover+.popup,
+  .icon:hover + .popup,
   .popup:hover {
     display: block !important;
   }
@@ -33,12 +33,13 @@
   }
 
   .text-eng {
-    font-family: Lato, 'Helvetica Neue', Arial, Helvetica, sans-serif;
+    font-family: Lato, "Helvetica Neue", Arial, Helvetica, sans-serif;
   }
-  
+
   ul.ui.list li:before {
     margin-right: -1.25rem;
   }
+
   ul.ui.list {
     margin-left: 0;
     margin-right: 1rem;
@@ -47,23 +48,21 @@
 
 <div class="icon-container">
   <i class="info circle icon large" />
-  <div
-    id="info-popup"
-    class="ui popup bottom right transition"
-    >
+  <div id="info-popup" class="ui popup bottom right transition">
     <div class="popup--header">راهنما</div>
     <div class="content">
-    <ul class="ui list">
-      <li value="*">
-      با دابل کلیک روی هر فیلد میتونید اون فیلد رو به حالت نوشتن تبدیل کنید و برای ثبت کلید
-      <span class="text-eng"> Enter </span>
-      رو بزنید یا روی هر بخش دیگه کلیک کنید.
-      </li>
-      <li>
-      با استفاده از دکمه‌های پایین جدول هم میتونید برای هفته‌های
-      زوج، فرد یا ثابت دروس رو وارد کنید.
-      </li>
-    </ul>
+      <ul class="ui list">
+        <li value="*">
+          با دابل کلیک روی هر فیلد میتونید اون فیلد رو به حالت نوشتن تبدیل کنید
+          و برای ثبت کلید
+          <span class="text-eng">Enter</span>
+          رو بزنید یا روی هر بخش دیگه کلیک کنید.
+        </li>
+        <li>
+          با استفاده از دکمه‌های پایین جدول هم میتونید برای هفته‌های زوج، فرد یا
+          ثابت دروس رو وارد کنید.
+        </li>
+      </ul>
     </div>
   </div>
 </div>
