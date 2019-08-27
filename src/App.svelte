d8dbc0817be27c61b86e5340cafacc87afe6b008
morteza behboodian <xeptore@gmail.com>
2019-08-27 09:47:25 +0430

    fix info tooltip and reset button layout

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
   import Info from './components/Info.svelte';
   import Table from './components/Table.svelte';
   import Buttons from './components/Buttons.svelte';
+  import ResetButton from './components/ResetButton.svelte';
 
   export let filter = '';
   export let rows = [];
@@ -23,20 +24,34 @@
   }
 
   span {
-    font-family: Lato, 'Helvetica Neue', Arial, Helvetica, sans-serif;
+    font-family: Lato, "Helvetica Neue", Arial, Helvetica, sans-serif;
     position: fixed;
     bottom: 0;
     display: inline-block;
     margin: 0 auto;
     text-align: center;
-    padding: .5rem;
+    padding: 0.5rem;
   }
 </style>
 
 <div class="ui container wrapper">
-  <Info/>
+  <div class="ui two column grid">
+    <div class="eight wide column">
+      <Info />
+    </div>
+    <div class="left aligned eight wide column">
+      <ResetButton />
+    </div>
+  </div>
   <Table {rows} {filter} on:change={handleTableCellChange} />
   <Buttons selected={filter} on:change={handleFilterChange} />
 </div>
 
-<span>made to <i dir="ltr" title="work!" class="cog icon"></i> by <strong><a target="_blank" href="https://github.com/xeptore">xeptore</a></strong></span>
+<span>
+  made to
+  <i dir="ltr" title="work!" class="cog icon" />
+  by
+  <strong>
+    <a target="_blank" href="https://github.com/xeptore">xeptore</a>
+  </strong>
+</span>
